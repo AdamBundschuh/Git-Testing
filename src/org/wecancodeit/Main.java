271e2e5de9d46f7e73ee494e791d4c9069f925e7
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-05-19 16:50:18 -0400

    Initial commit

---
+++ src/org/wecancodeit/Main.java
@@ -0,0 +1,8 @@
+package org.wecancodeit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git, is it me you're looking for?");
+    }
+}
---
+++ src/org/wecancodeit/SecondFile.java
@@ -0,0 +1,8 @@
+package org.wecancodeit;
+
+public class SecondFile {
+    public static void main(String[] args) {
+        System.out.println("Goodbye Cruel World!");
+
+    }
+}
