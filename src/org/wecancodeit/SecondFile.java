695b772c45bd35cb27d53e97870793057f9184dd
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-05-19 16:55:25 -0400

    Second push from GitBash

--- src/org/wecancodeit/SecondFile.java
+++ src/org/wecancodeit/SecondFile.java
@@ -3,6 +3,7 @@ package org.wecancodeit;
 public class SecondFile {
     public static void main(String[] args) {
         System.out.println("Goodbye Cruel World!");
+        System.out.println("Goodbye, World!");
 
     }
 }
